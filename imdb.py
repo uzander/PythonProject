6d51bf5d2e195572021e3ed39a1555dc89acad93
Uli Zander <uzander0@gmail.com>
2017-10-26 16:40:38 +0200

    full file

--- imdb.py
+++ imdb.py
@@ -1,19 +1,10 @@
 """Script to gather IMDB keywords from 2013's top grossing movies."""
 import sys
-
-URL = "http://www.imdb.com/search/title?at=0&sort=boxoffice_gross_us,desc&start=1&year=2013,2013"
-
+import requests
+from bs4 import BeautifulSoup
 import csv
 
-def main():
-    """Main entry point for the script."""
-    movies = get_top_grossing_movie_links(URL)
-    with open('output.csv', 'w') as output:
-        csvwriter = csv.writer(output)
-        for title, url in movies:
-            keywords = get_keywords_for_movie(
-                'http://www.imdb.com{}keywords/'.format(url))
-            csvwriter.writerow([title, keywords])
+URL = "http://www.imdb.com/search/title?at=0&sort=boxoffice_gross_us,desc&start=1&year=2013,2013"
 
 def get_top_grossing_movie_links(url):
     """Return a list of tuples containing the top grossing movies of 2013 and link to their IMDB
@@ -26,6 +17,8 @@ def get_top_grossing_movie_links(url):
             movies_list.append((movie_title, each_url['href']))
     return movies_list
 
+
+
 def get_keywords_for_movie(url):
     """Return a list of keywords associated with *movie*."""
     keywords = []
@@ -34,6 +27,17 @@ def get_keywords_for_movie(url):
     tables = soup.find_all('table', class_='dataTable')
     table = tables[0]
     return [td.text for tr in table.find_all('tr') for td in tr.find_all('td')]
+
+
+def main():
+    """Main entry point for the script."""
+    movies = get_top_grossing_movie_links(URL)
+    with open('output.csv', 'w') as output:
+        csvwriter = csv.writer(output)
+        for title, url in movies:
+            keywords = get_keywords_for_movie('http://www.imdb.com{}keywords/'.format(url))
+            csvwriter.writerow([title, keywords])
+
 
 if __name__ == '__main__':
     sys.exit(main())
